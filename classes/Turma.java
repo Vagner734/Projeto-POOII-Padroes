3ff8c0f0fb49e988a273cac97fa07db3b50c801a
Vagner Souza <vagnersantossouza734@gmail.com>
2025-03-01 10:52:15 -0300

    Update Turma.java

--- classes/Turma.java
+++ classes/Turma.java
@@ -7,117 +7,138 @@ public class Turma {
 
     private String identificacao;
     private Curso curso;
-    private Semestre semestre;  // Alterado para o tipo Semestre
-    private ArrayList<Professor> professores = new ArrayList<>();
-    private ArrayList<AlunoTurma> alunos = new ArrayList<>();
-    private List<Log> logs = new ArrayList<>();
+    private Semestre semestre;
+    private ArrayList<Professor> professores;
+    private ArrayList<AlunoTurma> alunosTurma;
+    private final List<Log> logs;
 
-    // Construtores
     public Turma() {
-    }
-
-    public Turma(String identificacao, Curso curso, Semestre semestre) {
-        this.identificacao = identificacao;
-        this.curso = curso;
-        this.semestre = semestre;
+        this.professores = new ArrayList<>();
+        this.alunosTurma = new ArrayList<>();
         this.logs = new ArrayList<>();
     }
 
-    // Getters e Setters
+    
+    public Turma(String identificacao, Curso curso, Semestre semestre) {
+        this.identificacao = identificacao != null ? identificacao : "";
+        this.curso = curso;
+        this.semestre = semestre;
+        this.professores = new ArrayList<>();
+        this.alunosTurma = new ArrayList<>();
+        this.logs = new ArrayList<>();
+    }
+
+    
     public String getIdentificacao() {
         return identificacao;
     }
 
+    
     public void setIdentificacao(String identificacao) {
-        this.identificacao = identificacao;
+        this.identificacao = identificacao != null ? identificacao : "";
     }
 
+    
     public Curso getCurso() {
         return curso;
     }
 
+    
     public void setCurso(Curso curso) {
         this.curso = curso;
     }
 
+    
     public Semestre getSemestre() {
         return semestre;
     }
 
+    
     public void setSemestre(Semestre semestre) {
         this.semestre = semestre;
     }
 
+    
     public ArrayList<Professor> getProfessores() {
         return professores;
     }
 
+    
     public void setProfessores(ArrayList<Professor> professores) {
-        this.professores = professores;
+        this.professores = professores != null ? professores : new ArrayList<>();
     }
 
+    
     public ArrayList<AlunoTurma> getAlunosTurma() {
-        return alunos;
+        return alunosTurma;
     }
 
-    public void setAlunos(ArrayList<AlunoTurma> alunos) {
-        this.alunos = alunos;
+    
+    public void setAlunosTurma(ArrayList<AlunoTurma> alunosTurma) {
+        this.alunosTurma = alunosTurma != null ? alunosTurma : new ArrayList<>();
     }
 
-    // Métodos
+    
     public void adicionarProfessor(Professor professor) {
-        professor.setCoodernador(false);
-        professores.add(professor);
+        if (professor != null) {
+            professores.add(professor);
+        }
     }
 
+    
     public void adicionarAluno(AlunoTurma aluno) {
-        alunos.add(aluno);
+        if (aluno != null) {
+            alunosTurma.add(aluno);
+        }
     }
 
-    public void setEstatica() {
-        System.out.println("Estatísticas das notas dos alunos:");
-        int totalAprovados = 0;
-        int totalReprovados = 0;
-        int totalRecuperacao = 0;
-
-        for (AlunoTurma aluno : alunos) {
-            String nome = aluno.getAluno().getNome();
-            double media = aluno.getNota().calcularMedia();
-
-            System.out.printf("Nome: %s, Média: %.2f\n", nome, media);
-            if (media < 2.5) {
-                totalReprovados++;
-            } else if (media < 7) {
-                totalRecuperacao++;
-            } else {
-                totalAprovados++;
+    
+    public void exibirEstatistica() {
+        System.out.println("Estatísticas da Turma " + identificacao + ":");
+        int aprovados = 0, reprovados = 0, recuperacao = 0;
+        for (AlunoTurma at : alunosTurma) {
+            if (at != null && at.getNota() != null) {
+                double media = at.getNota().calcularMedia();
+                System.out.printf("%s: %.2f\n", at.getAluno().getNome(), media);
+                if (media < 2.5) {
+                    reprovados++; 
+                }else if (media < 7) {
+                    recuperacao++; 
+                }else {
+                    aprovados++;
+                }
             }
         }
-        
-        
-        System.out.println("Total de aprovados: " + totalAprovados);
-        System.out.println("Total de reprovados: " + totalReprovados);
-        System.out.println("Total de recuperação: " + totalRecuperacao);
+        System.out.println("Aprovados: " + aprovados + ", Reprovados: " + reprovados + ", Recuperação: " + recuperacao);
     }
 
     public void alterarNotaEstudante(Professor coordenador, AlunoTurma alunoTurma, Nota novaNota) {
-        if (coordenador.getCoordenador()) {
+        if (coordenador != null && coordenador.isCoordenador() && alunoTurma != null && novaNota != null) {
+            Nota antiga = alunoTurma.getNota();
             alunoTurma.setNota(novaNota);
+            logs.add(new Log(coordenador, antiga, novaNota));
+            System.out.println("Nota alterada com sucesso!");
         } else {
-            System.out.println("Somente coordenadores podem alterar notas.");
+            System.out.println("Apenas coordenadores podem alterar notas ou parâmetros inválidos.");
         }
     }
 
     public void exibirDados() {
         System.out.println("Turma: " + identificacao);
-        System.out.println("Semestre: " + (semestre != null ? semestre.getNome() : "Não definido"));
+        System.out.println("Curso: " + (curso != null ? curso.getNomeCurso() : "Nenhum"));
+        System.out.println("Semestre: " + (semestre != null ? semestre.getNome() : "Nenhum"));
         System.out.println("Professores:");
-        for (Professor professor : professores) {
-            professor.exibirDados();
+        for (Professor p : professores) {
+            if (p != null) {
+                p.exibirDados();
+            }
         }
+        
         System.out.println("Alunos:");
-        for (AlunoTurma aluno : alunos) {
-            aluno.getAluno().exibirDados();
+        for (AlunoTurma at : alunosTurma) {
+            if (at != null && at.getAluno() != null) {
+                at.getAluno().exibirDados();
+            }
         }
     }
 }
