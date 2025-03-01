4a5cc10b1545d7ce93b071fffca1df6512de5f3f
Vagner Souza <vagnersantossouza734@gmail.com>
2025-03-01 10:49:35 -0300

    Update Professor.java

--- classes/Professor.java
+++ classes/Professor.java
@@ -2,50 +2,48 @@ package classes;
 
 public class Professor extends Pessoa {
 
- private String siape;
- private boolean coordenador = false;
- private String senha;
+    private String siape;
+    private boolean coordenador = false;
+    private String senha;
 
- public Professor(){
- super();
-  }
-  
-  public Professor( String nome, String cpf, String telefone, String endereco, String siape, String senha){
-  super(nome, cpf, telefone, endereco);
-  this.senha  =  senha;
-  this.siape = siape;
-  }
- 
- 
+    public Professor() {
+        super();
+    }
 
-  public String getSiape(){
-    return siape;
-  }
+    public Professor(String nome, String cpf, String telefone, String endereco, String siape, String senha) {
+        super(nome, cpf, telefone, endereco);
+        this.siape = siape;
+        this.senha = senha;
+    }
 
-  public void setSiape( String siape){
-    this.siape = siape;
-  }
+    public String getSiape() {
+        return siape;
+    }
 
-  public String getSenha(){
-    return senha;
-  }
+    public void setSiape(String siape) {
+        this.siape = siape;
+    }
 
-  public void setSenha(String senha){
-    this.senha = senha;
-  }
+    public String getSenha() {
+        return senha;
+    }
 
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-  public boolean getCoordenador(){
-    return coordenador;
-  }
+    public boolean isCoordenador() {
+        return coordenador;
+    }
 
-  public void setCoodernador( boolean coordenador){
-    this.coordenador = coordenador;
-  }
+    public void setCoordenador(boolean coordenador) {
+        this.coordenador = coordenador;
+    }
 
-  public void exibirDados(){
-    super.exibirDados();
-    System.out.println("SIAPE: " + siape);
-  }
-
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("SIAPE: " + siape);
+        System.out.println("Coordenador: " + (coordenador ? "Sim" : "Não"));
+    }
 }
