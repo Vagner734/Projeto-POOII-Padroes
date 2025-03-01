a2d8bb68f3478d4dc6ed91e28926c562909b0630
Vagner Souza <vagnersantossouza734@gmail.com>
2025-03-01 10:50:38 -0300

    Update Semestre.java

--- classes/Semestre.java
+++ classes/Semestre.java
@@ -1,57 +1,60 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Semestre {
+
     private String nome;
     private boolean ativo;
-    private List<Observer> observers = new ArrayList<>();
+    private final List<Observer> observers;
 
     public Semestre(String nome, boolean ativo) {
-        this.nome = nome;
+        this.nome = nome != null ? nome : "";
         this.ativo = ativo;
+        this.observers = new ArrayList<>();
     }
 
+    
     public String getNome() {
         return nome;
     }
 
+    
     public void setNome(String nome) {
-        this.nome = nome;
+        this.nome = nome != null ? nome : "";
     }
 
+    
     public boolean isAtivo() {
         return ativo;
     }
 
+    
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
         notifyObservers();
     }
 
+    
     public void addObserver(Observer observer) {
-        observers.add(observer);
-    }
-
-    public void removeObserver(Observer observer) {
-        observers.remove(observer);
-    }
-
-    private void notifyObservers() {
-        for (Observer observer : observers) {
-            observer.update();
+        if (observer != null && !observers.contains(observer)) {
+            observers.add(observer);
         }
     }
 
-    public void addObserver (Observer observer)
-    (
-        observadores.add();
-    )
     
-   public void removerObservador (Observer observer){
-    observadores.remover(Observer);
-   }
+    public void removeObserver(Observer observer) {
+        if (observer != null) {
+            observers.remove(observer);
+        }
+    }
 
-
-    public void natifi (){
-        for (int i = 0 ; < observadores.size); i++) {
-            observadores.get(i).update(ativo); "observer"
+    private void notifyObservers() {
+        for (Observer o : new ArrayList<>(observers)) {
+            if (o != null) {
+                o.update(ativo);
+            }
         }
     }
 }
